81f484f9f89ee6b36c166bbc9c20e5dc5e043157
mari-pin <mirtraffik@gmail.com>
2025-04-02 11:23:39 +0200

    modificados con JpaSpecification

--- tutorial/src/main/java/com/ccsw/tutorial/game/GameServiceImp.java
+++ tutorial/src/main/java/com/ccsw/tutorial/game/GameServiceImp.java
@@ -2,11 +2,13 @@ package com.ccsw.tutorial.game;
 
 import com.ccsw.tutorial.author.AuthorService;
 import com.ccsw.tutorial.category.CategoryService;
+import com.ccsw.tutorial.common.criteria.SearchCriteria;
 import com.ccsw.tutorial.game.model.Game;
 import com.ccsw.tutorial.game.model.GameDto;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,10 +31,16 @@ public class GameServiceImp implements GameService {
     /**
      * {@inheritDoc}
      */
+
     @Override
     public List<Game> find(String title, Long idCategory) {
 
-        return (List<Game>) this.gameRepository.findAll();
+        GameSpecification titleSpec = new GameSpecification(new SearchCriteria("title", ":", title));
+        GameSpecification categorySpec = new GameSpecification(new SearchCriteria("category.id", ":", idCategory));
+
+        Specification<Game> spec = Specification.where(titleSpec).and(categorySpec);
+
+        return this.gameRepository.findAll(spec);
     }
 
     /**
