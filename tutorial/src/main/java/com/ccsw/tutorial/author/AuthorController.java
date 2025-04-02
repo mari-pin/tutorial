20003f31c0e59d85dccfe021be6090ab1893f39e
mari-pin <mirtraffik@gmail.com>
2025-04-02 16:11:06 +0200

    eliminados metodos mal creados

--- tutorial/src/main/java/com/ccsw/tutorial/author/AuthorController.java
+++ tutorial/src/main/java/com/ccsw/tutorial/author/AuthorController.java
@@ -11,10 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 /**
@@ -34,6 +31,20 @@ public class AuthorController {
     ModelMapper mapper;
 
     /**
+     * Recupera un listado de autores {@link Author}
+     *
+     * @return {@link List} de {@link AuthorDto}
+     */
+    @Operation(summary = "Find", description = "Method that return a list of Authors")
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<AuthorDto> findAll() {
+
+        List<Author> authors = this.authorService.findAll();
+
+        return authors.stream().map(e -> mapper.map(e, AuthorDto.class)).collect(Collectors.toList());
+    }
+
+    /**
      * Método para recuperar un listado paginado de {@link Author}
      *
      * @param dto dto de búsqueda
@@ -42,135 +53,16 @@ public class AuthorController {
     @Operation(summary = "Find Page", description = "Method that return a page of Authors")
     @RequestMapping(path = "", method = RequestMethod.POST)
     public Page<AuthorDto> findPage(@RequestBody AuthorSearchDto dto) {
-        try {
-            Page<Author> page = this.authorService.findPage(dto);
 
-            return new PageImpl<>(page.getContent().stream().map(e -> mapper.map(e, AuthorDto.class)).collect(Collectors.toList()), page.getPageable(), page.getTotalElements());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return new PageImpl<>(new List<AuthorDto>() {
-            @Override
-            public int size() {
-                return 0;
-            }
+        Page<Author> page = this.authorService.findPage(dto);
 
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<AuthorDto> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(AuthorDto authorDto) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends AuthorDto> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends AuthorDto> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public AuthorDto get(int index) {
-                return null;
-            }
-
-            @Override
-            public AuthorDto set(int index, AuthorDto element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, AuthorDto element) {
-
-            }
-
-            @Override
-            public AuthorDto remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<AuthorDto> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<AuthorDto> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<AuthorDto> subList(int fromIndex, int toIndex) {
-                return List.of();
-            }
-        });
+        return new PageImpl<>(page.getContent().stream().map(e -> mapper.map(e, AuthorDto.class)).collect(Collectors.toList()), page.getPageable(), page.getTotalElements());
     }
 
     /**
      * Método para crear o actualizar un {@link Author}
      *
-     * @param id PK de la entidad
+     * @param id  PK de la entidad
      * @param dto datos de la entidad
      */
     @Operation(summary = "Save or Update", description = "Method that saves or updates a Author")
