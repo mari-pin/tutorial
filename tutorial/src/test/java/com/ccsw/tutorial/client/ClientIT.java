0c9cda48d8ac66ac150d15486fc2d389142e7087
mari-pin <mirtraffik@gmail.com>
2025-04-03 15:53:38 +0200

    configurado juniy mockito

---
+++ tutorial/src/test/java/com/ccsw/tutorial/client/ClientIT.java
@@ -0,0 +1,19 @@
+package com.ccsw.tutorial.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+
+public class ClientIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+}
---
+++ tutorial/src/test/java/com/ccsw/tutorial/client/ClientTest.java
@@ -0,0 +1,9 @@
+package com.ccsw.tutorial.client;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+
+public class ClientTest {
+}
