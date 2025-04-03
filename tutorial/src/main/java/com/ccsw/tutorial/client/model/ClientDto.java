8b5a058b411fbecee5491a3c8c28866ea044421e
mari-pin <mirtraffik@gmail.com>
2025-04-03 15:12:42 +0200

    creado modelo ClienteDto con los metodos get t set

---
+++ tutorial/src/main/java/com/ccsw/tutorial/client/model/ClientDto.java
@@ -0,0 +1,24 @@
+package com.ccsw.tutorial.client.model;
+
+public class ClientDto {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return this.getId();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+
+    }
+
+    public String getName() {
+        return this.getName();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
