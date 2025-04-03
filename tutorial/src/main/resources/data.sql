3ad0cce14d40d97f9ce105ff7e7d55088a9b1f64
mari-pin <mirtraffik@gmail.com>
2025-04-03 15:24:34 +0200

    añadidos insert de los clientes

--- tutorial/src/main/resources/data.sql
+++ tutorial/src/main/resources/data.sql
@@ -21,3 +21,8 @@ INSERT INTO game(title, age, category_id, author_id) VALUES ('1920: Wall Street'
 INSERT INTO game(title, age, category_id, author_id) VALUES ('Barrage', '14', 1, 3);
 INSERT INTO game(title, age, category_id, author_id) VALUES ('Los viajes de Marco Polo', '12', 1, 3);
 INSERT INTO game(title, age, category_id, author_id) VALUES ('Azul', '8', 3, 5);
+
+
+INSERT INTO client(name) VALUES ('Ana');
+INSERT INTO client(name) VALUES ('Amparo');
+INSERT INTO client(name) VALUES ('Fede');
