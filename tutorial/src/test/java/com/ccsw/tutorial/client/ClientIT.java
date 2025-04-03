8269ba388f833f39cd223be2526e11bec0080b13
mari-pin <mirtraffik@gmail.com>
2025-04-03 17:07:29 +0200

    test creados y funcionando

--- tutorial/src/test/java/com/ccsw/tutorial/client/ClientIT.java
+++ tutorial/src/test/java/com/ccsw/tutorial/client/ClientIT.java
@@ -1,19 +1,43 @@
 package com.ccsw.tutorial.client;
 
+import com.ccsw.tutorial.client.model.ClientDto;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 
 public class ClientIT {
+    public static final String LOCALHOST = "http://localhost:";
+    public static final String SERVICE_PATH = "/client";
 
     @LocalServerPort
     private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    ParameterizedTypeReference<List<ClientDto>> responseType = new ParameterizedTypeReference<List<ClientDto>>() {
+    };
+
+    @Test
+    public void findAllShouldReturnAllCategories() {
+
+        ResponseEntity<List<ClientDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.GET, null, responseType);
+
+        assertNotNull(response);
+        assertEquals(3, response.getBody().size());
+    }
 }
--- tutorial/src/test/java/com/ccsw/tutorial/client/ClientTest.java
+++ tutorial/src/test/java/com/ccsw/tutorial/client/ClientTest.java
@@ -1,9 +1,39 @@
 package com.ccsw.tutorial.client;
 
+import com.ccsw.tutorial.client.model.Client;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 
 public class ClientTest {
+
+    @Mock
+    private ClientRepository clientRepository;
+    @InjectMocks
+    private ClientServiceImp clientService;
+
+    @Test
+    public void findAllShouldReturnAllClients() {
+        List<Client> list = new ArrayList<>();
+        list.add(mock(Client.class));
+
+        when(clientRepository.findAll()).thenReturn(list);
+        List<Client> clients = clientService.findAll();
+
+        assertNotNull(clients);
+        assertEquals(1, clients.size());
+
+    }
 }
