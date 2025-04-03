d64e7a639e119b7afae1702c897281dbc015645d
mari-pin <mirtraffik@gmail.com>
2025-04-03 15:16:10 +0200

    creado servicio con metodos necesarios

---
+++ tutorial/src/main/java/com/ccsw/tutorial/client/ClientService.java
@@ -0,0 +1,36 @@
+package com.ccsw.tutorial.client;
+
+import com.ccsw.tutorial.client.model.Client;
+import com.ccsw.tutorial.client.model.ClientDto;
+
+import java.util.List;
+
+/**
+ * author ccsw
+ */
+
+public interface ClientService {
+
+    /**
+     * Metodo parar recuperar todas los clientes
+     *
+     * @return {@link List} de {@link Client}
+     **/
+    List<Client> findAll();
+
+    /**
+     * Método para crear o actualizar un {@link Client}
+     *
+     * @param id PK de la entidad
+     * @param dto datos de la entidad
+     */
+    void save(Long id, ClientDto dto);
+
+    /**
+     * Método para borrar un {@link  Client}
+     *
+     * @param id PK de la entidad
+     */
+    void delete(Long id) throws Exception;
+
+}
