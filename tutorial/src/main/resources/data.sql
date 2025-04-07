a16df3f303558f40c88877fb3ad760519276dd0f
mari-pin <mirtraffik@gmail.com>
2025-04-07 13:50:56 +0200

    añadidos prestamos a la bbdd

--- tutorial/src/main/resources/data.sql
+++ tutorial/src/main/resources/data.sql
@@ -26,3 +26,13 @@ INSERT INTO game(title, age, category_id, author_id) VALUES ('Azul', '8', 3, 5);
 INSERT INTO client(name) VALUES ('Ana');
 INSERT INTO client(name) VALUES ('Amparo');
 INSERT INTO client(name) VALUES ('Fede');
+
+
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (1, 1, '2025-04-07', '2025-04-09');
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (2, 2, '2025-04-07', '2025-04-11');
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (2, 1, '2025-04-08', '2025-04-12');
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (3, 3, '2025-04-07', '2025-04-09');
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (2, 1, '2025-04-09', '2025-04-15');
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (4, 3, '2025-04-10', '2025-04-16');
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (3, 1, '2025-04-12', '2025-04-16');
+INSERT INTO prestamo(game_id, client_id, initdate, enddate) VALUES (5, 2, '2025-04-08', '2025-04-19');
