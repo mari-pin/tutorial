63b8d30056e147334af67c760e060bfc95ba7dc9
mari-pin <mirtraffik@gmail.com>
2025-04-08 16:26:57 +0200

    separadas los metodos de la lista de prestamos y los prestamos

--- tutorial/src/main/java/com/ccsw/tutorial/prestamo/PrestamoRepository.java
+++ tutorial/src/main/java/com/ccsw/tutorial/prestamo/PrestamoRepository.java
@@ -5,11 +5,22 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PrestamoRepository extends CrudRepository<Prestamo, Long>, JpaSpecificationExecutor<Prestamo> {
+import java.util.List;
+
+public interface PrestamoRepository extends CrudRepository<Prestamo, Long>, JpaSpecificationExecutor<Prestamo>, JpaRepository<Prestamo, Long> {
+    /**
+     * Método para recuperar un listado paginado de {@link Prestamo}
+     *
+     * @param pageable pageable
+     * @return {@link Page} de {@link Prestamo}
+     */
+    Page<Prestamo> findAll(Pageable pageable);
+
     @Override
     @EntityGraph(attributePaths = { "game", "client" })
-    Page<Prestamo> findAll(Specification<Prestamo> spec, Pageable pageable);
+    List<Prestamo> findAll(Specification<Prestamo> spec);
 }
