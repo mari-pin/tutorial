71a2d1c784bbfc3aa7f0b21cfd449b8f43cd10ba
mari-pin <mirtraffik@gmail.com>
2025-04-08 16:24:45 +0200

    eliminados metodos de no necesarios

--- tutorial/src/main/java/com/ccsw/tutorial/prestamo/PrestamoServiceImp.java
+++ tutorial/src/main/java/com/ccsw/tutorial/prestamo/PrestamoServiceImp.java
@@ -1,8 +1,10 @@
 package com.ccsw.tutorial.prestamo;
 
 import com.ccsw.tutorial.client.ClientService;
+import com.ccsw.tutorial.client.model.ClientDto;
 import com.ccsw.tutorial.common.criteria.SearchCriteria;
 import com.ccsw.tutorial.game.GameService;
+import com.ccsw.tutorial.game.model.GameDto;
 import com.ccsw.tutorial.prestamo.model.Prestamo;
 import com.ccsw.tutorial.prestamo.model.PrestamoDto;
 import com.ccsw.tutorial.prestamo.model.PrestamoSearchDto;
@@ -11,138 +13,129 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
 @Transactional
-
 public class PrestamoServiceImp implements PrestamoService {
     @Autowired
     PrestamoRepository prestamoRepository;
 
     @Autowired
-    ClientService clientService;
-    @Autowired
     GameService gameService;
 
-    @Override
-    public List<Prestamo> findAll() {
-        return (List<Prestamo>) this.prestamoRepository.findAll();
-    }
+    @Autowired
+    ClientService clientService;
 
     @Override
-    public void save(Long id, PrestamoDto dto) {
-        Prestamo prestamo;
+    public List<Prestamo> find(Long idClient, Long idGame, LocalDate fechaInicio) {
+        PrestamoSpecification clientSpec = new PrestamoSpecification(new SearchCriteria("client.id", ":", idClient));
+        PrestamoSpecification gameSpec = new PrestamoSpecification(new SearchCriteria("game.id", ":", idGame));
+        PrestamoSpecification fechaSpec = new PrestamoSpecification(new SearchCriteria("fechainicio", ">", fechaInicio));
 
-        prestamo = new Prestamo();
-        BeanUtils.copyProperties(dto, prestamo, "id", "game", "client");
+        Specification<Prestamo> spec = Specification.where(clientSpec).and(gameSpec).and(fechaSpec);
 
-        prestamo.setGame(gameService.get(dto.getGame().getId()));
-        prestamo.setClient(clientService.get(dto.getClient().getId()));
-
-        if (areGamesOverlapping(dto)) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "El juego está prestado en esta fecha");
-        } else if (areClientOverlapping(dto)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El cliente ya tiene 2 préstamos en estas fechas");
-        } else {
-            this.prestamoRepository.save(prestamo);
-        }
+        return this.prestamoRepository.findAll(spec);
     }
 
+    /**
+     *
+     * @param gameId
+     * @param clientId
+     * @param date
+     * @param dto dto de búsqueda
+     * @return
+     */
     @Override
     public Page<Prestamo> findPage(Long gameId, Long clientId, LocalDate date, PrestamoSearchDto dto) {
-        PrestamoSpecification idGameSpec = new PrestamoSpecification(new SearchCriteria("game.id", ":", gameId));
-        PrestamoSpecification idClientSpec = new PrestamoSpecification(new SearchCriteria("client.id", ":", clientId));
-        PrestamoSpecification dateEndSpec = new PrestamoSpecification(new SearchCriteria("endDate", "lessThanOrEqualTo", date));
-        PrestamoSpecification dateStartSpec = new PrestamoSpecification(new SearchCriteria("startDate", "greaterThanOrEqualTo", date));
+        return null;
+    }
 
-        Specification<Prestamo> spec = Specification.where(idGameSpec).and(idClientSpec).and(dateEndSpec).and(dateStartSpec);
+    @Override
+    public Page<Prestamo> findPage(PrestamoSearchDto dto) {
+        return this.prestamoRepository.findAll(dto.getPageable().getPageable());
+    }
 
-        return this.prestamoRepository.findAll(spec, dto.getPageable().getPageable());
+    /**
+     *
+     * @return
+     */
+    @Override
+    public List<Prestamo> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void save(Long id, PrestamoDto data) {
+        Prestamo prestamo;
+        Boolean juegoPrestado;
+        juegoPrestado = this.findByGame(data.getGame(), data.getInitDate(), data.getEndDate(), data);
+
+        Boolean clienteJuego;
+        clienteJuego = this.findyByClientGame(data.getClient(), data);
+
+        if (id == null) {
+
+            if (data.getInitDate().isBefore(data.getInitDate())) {
+                System.out.println("La fecha de fin no puede ser anterior a la fecha de inicio");
+            }
+            if (!juegoPrestado) {
+                System.out.println("Ya hay un juego prestado");
+            }
+            if (clienteJuego) {
+                System.out.println("Ya has reservado mas de 2 juegos");
+            } else {
+                prestamo = new Prestamo();
+                BeanUtils.copyProperties(data, prestamo, "id", "game", "client");
+
+                prestamo.setGame(gameService.get(data.getGame().getId()));
+                prestamo.setClient(clientService.get(data.getClient().getId()));
+
+                this.prestamoRepository.save(prestamo);
+            }
+
+        } else {
+            prestamo = this.prestamoRepository.findById(id).orElse(null);
+            BeanUtils.copyProperties(data, prestamo, "id", "game", "client");
+
+            prestamo.setGame(gameService.get(data.getGame().getId()));
+            prestamo.setClient(clientService.get(data.getClient().getId()));
+
+            this.prestamoRepository.save(prestamo);
+        }
+
     }
 
     @Override
     public void delete(Long id) throws Exception {
+
         if (this.prestamoRepository.findById(id).orElse(null) == null) {
-            throw new Exception("Not exists");
+            throw new Exception("No existe");
         }
 
         this.prestamoRepository.deleteById(id);
     }
 
-    @Override
-    public boolean areGamesOverlapping(PrestamoDto dto) {
-        Long gameId = dto.getGame().getId();
-        Long clientId = dto.getClient().getId();
-        LocalDate startDate = dto.getInitDate();
-        LocalDate endDate = dto.getEndDate();
-
-        List<Prestamo> prestamos = this.findAll();
-
-        for (Prestamo prestamo : prestamos) {
-            if (prestamo.getGame().getId() == gameId) {
-                LocalDate endDatePrestamo = prestamo.getEndDate();
-                LocalDate initDatePrestamo = prestamo.getInitDate();
-
-                boolean isOverLapping = (startDate.isBefore(endDatePrestamo) && endDate.isAfter(initDatePrestamo) || (initDatePrestamo.isBefore(endDate) && endDatePrestamo.isAfter(startDate)));
-
-                if (isOverLapping) {
-                    return true;
-                }
+    public boolean findByGame(GameDto game, LocalDate fechaInicio, LocalDate fechaFin, PrestamoDto dto) {
+        List<Prestamo> prestamosExistentes = find(null, game.getId(), null);
+        if (!prestamosExistentes.isEmpty()) {
+            if (!(dto.getInitDate().isBefore(fechaInicio) || fechaInicio.isAfter(dto.getEndDate()))) {
+                return false;
             }
         }
-
-        return false;
+        return true;
     }
 
-    @Override
-    public boolean areClientOverlapping(PrestamoDto dto) {
-        Long clientId = dto.getClient().getId();
-        LocalDate initDate = dto.getInitDate();
-        LocalDate endDate = dto.getEndDate();
+    public boolean findyByClientGame(ClientDto client, PrestamoDto dto) {
+        List<Prestamo> prestamosCliente = find(client.getId(), null, dto.getInitDate());
 
-        List<Prestamo> prestamos = this.findAll();
-
-        for (LocalDate date = initDate; !date.isAfter(endDate); date = date.plusDays(1)) {
-            int prestamoCount = 0;
-            for (Prestamo prestamo : prestamos) {
-                if (prestamo.getClient().getId().equals(clientId) && !prestamo.getEndDate().isBefore(date) && !prestamo.getInitDate().isAfter(date)) {
-                    prestamoCount++;
-                }
-            }
-            if (prestamoCount >= 2) {
-                return true;
-            }
+        if (prestamosCliente.size() >= 2) {
+            return true;
         }
         return false;
     }
 
-    //implementados ahora
-
-    /**
-     *
-     * @param dto
-     * @return
-     */
-    @Override
-    public Page<Prestamo> findPage(PrestamoSearchDto dto) {
-        return null;
-    }
-
-    /**
-     *
-     * @param idClient
-     * @param idGame
-     * @param initDate
-     * @return
-     */
-    @Override
-    public List<Prestamo> find(Long idClient, Long idGame, LocalDate initDate) {
-        return List.of();
-    }
 }
